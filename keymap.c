47f2e40f227d9d6e8d32ce0b31ec041788a5de98
Morphy Kuffour <morphy.kuffour@uconn.edu>
2025-02-02 17:17:02 -0500

    cleaned up code

--- keymap.c
+++ keymap.c
@@ -149,36 +149,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 #endif
             }
             break;
-        // case JIGGLE:
-        //     if (record->event.pressed) {
-        //         static deferred_token token = INVALID_DEFERRED_TOKEN;
-        //         static report_mouse_t report = {0};
-        //         if (token) {
-        //             // If jiggler is currently running, stop when any key is pressed.
-        //             cancel_deferred_exec(token);
-        //             token = INVALID_DEFERRED_TOKEN;
-        //             report = (report_mouse_t){};  // Clear the mouse.
-        //             host_mouse_send(&report);
-        //
-        //         } else if (keycode == JIGGLE) {
-        //
-        //             uint32_t jiggler_callback(uint32_t trigger_time, void* cb_arg) {
-        //                 // Deltas to move in a circle of radius 20 pixels over 32 frames.
-        //                 static const int8_t deltas[32] = {
-        //                     0, -1, -2, -2, -3, -3, -4, -4, -4, -4, -3, -3, -2, -2, -1, 0,
-        //                     0, 1, 2, 2, 3, 3, 4, 4, 4, 4, 3, 3, 2, 2, 1, 0};
-        //                 static uint8_t phase = 0;
-        //                 // Get x delta from table and y delta by rotating a quarter cycle.
-        //                 report.x = deltas[phase];
-        //                 report.y = deltas[(phase + 8) & 31];
-        //                 phase = (phase + 1) & 31;
-        //                 host_mouse_send(&report);
-        //                 return 16;  // Call the callback every 16 ms.
-        //             }
-        //             token = defer_exec(1, jiggler_callback, NULL);  // Schedule callback.
-        //         }
-        //     }
-        //     break;
         case KC_BSPC: {
             static uint16_t registered_key = KC_NO;
             if (record->event.pressed) {  // On key press.
