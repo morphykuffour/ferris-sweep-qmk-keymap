ad93317a1ddb8911c10410f3296bec7cc9616814
Morphy Kuffour <morphy.kuffour@uconn.edu>
2025-02-02 17:14:45 -0500

    added JIGGLE to keymap

--- keymap.c
+++ keymap.c
@@ -10,6 +10,7 @@
 enum custom_keycodes {
     CKC_COPY = SAFE_RANGE,
     CKC_PASTE,
+    JIGGLE,
     SMTD_KEYCODES_BEGIN,
     CKC_Z, // reads as C(ustom) + KC_A
     CKC_X,
@@ -62,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     QK_GESC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
-    KC_TRNS, KC_TRNS, CKC_COPY, KC_BTN1, CKC_PASTE,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_ENT,
+    JIGGLE, KC_TRNS, CKC_COPY, KC_BTN1, CKC_PASTE,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_ENT,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
@@ -82,6 +83,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+    if (record->event.pressed) {
+        static deferred_token token = INVALID_DEFERRED_TOKEN;
+        static report_mouse_t report = {0};
+        if (token) {
+            // If jiggler is currently running, stop when any key is pressed.
+            cancel_deferred_exec(token);
+            token = INVALID_DEFERRED_TOKEN;
+            report = (report_mouse_t){};  // Clear the mouse.
+            host_mouse_send(&report);
+
+        } else if (keycode == JIGGLE) {
+
+            uint32_t jiggler_callback(uint32_t trigger_time, void* cb_arg) {
+                // Deltas to move in a circle of radius 20 pixels over 32 frames.
+                static const int8_t deltas[32] = {
+                    0, -1, -2, -2, -3, -3, -4, -4, -4, -4, -3, -3, -2, -2, -1, 0,
+                    0, 1, 2, 2, 3, 3, 4, 4, 4, 4, 3, 3, 2, 2, 1, 0};
+                static uint8_t phase = 0;
+                // Get x delta from table and y delta by rotating a quarter cycle.
+                report.x = deltas[phase];
+                report.y = deltas[(phase + 8) & 31];
+                phase = (phase + 1) & 31;
+                host_mouse_send(&report);
+                return 16;  // Call the callback every 16 ms.
+            }
+            token = defer_exec(1, jiggler_callback, NULL);  // Schedule callback.
+        }
+    }
+
     if (!process_smtd(keycode, record)) {
         return false;
     }
@@ -118,6 +149,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 #endif
             }
             break;
+        // case JIGGLE:
+        //     if (record->event.pressed) {
+        //         static deferred_token token = INVALID_DEFERRED_TOKEN;
+        //         static report_mouse_t report = {0};
+        //         if (token) {
+        //             // If jiggler is currently running, stop when any key is pressed.
+        //             cancel_deferred_exec(token);
+        //             token = INVALID_DEFERRED_TOKEN;
+        //             report = (report_mouse_t){};  // Clear the mouse.
+        //             host_mouse_send(&report);
+        //
+        //         } else if (keycode == JIGGLE) {
+        //
+        //             uint32_t jiggler_callback(uint32_t trigger_time, void* cb_arg) {
+        //                 // Deltas to move in a circle of radius 20 pixels over 32 frames.
+        //                 static const int8_t deltas[32] = {
+        //                     0, -1, -2, -2, -3, -3, -4, -4, -4, -4, -3, -3, -2, -2, -1, 0,
+        //                     0, 1, 2, 2, 3, 3, 4, 4, 4, 4, 3, 3, 2, 2, 1, 0};
+        //                 static uint8_t phase = 0;
+        //                 // Get x delta from table and y delta by rotating a quarter cycle.
+        //                 report.x = deltas[phase];
+        //                 report.y = deltas[(phase + 8) & 31];
+        //                 phase = (phase + 1) & 31;
+        //                 host_mouse_send(&report);
+        //                 return 16;  // Call the callback every 16 ms.
+        //             }
+        //             token = defer_exec(1, jiggler_callback, NULL);  // Schedule callback.
+        //         }
+        //     }
+        //     break;
         case KC_BSPC: {
             static uint16_t registered_key = KC_NO;
             if (record->event.pressed) {  // On key press.
